7fe37f5885906288f9dba3ac7903f89e6570a636
Diego <diegogalvaoamorim@gmail.com>
2025-04-03 22:14:49 -0300

    diagrama de classe

--- src/main/java/guitarras/acme/model/Corda.java
+++ src/main/java/guitarras/acme/model/Corda.java
@@ -1,9 +1,6 @@
 package guitarras.acme.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Corda extends DefaultEntity{
